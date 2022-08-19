53d35e27a7504a294ebc3b9475c3cb470f61bdca
malinatrash <malinatrashh@gmail.com>
2022-08-19 18:12:50 +0800

    fixed

--- StateAndDataFlow/UserManager.swift
+++ StateAndDataFlow/UserManager.swift
@@ -12,9 +12,6 @@ class UserManager: ObservableObject {
     
     @Published var user = User()
     
-    var nameIsValid: Bool {
-        user.name.count >= 3
-    }
     init() {}
     
     init(user: User) {
